9c31ba31d2e723fbba2d517c592e15a3bacbef7e
nbost130 <nbost130@gmail.com>
2023-01-13 16:40:23 -0500

    Update Ender-3.js

--- builds/Ender-3.js
+++ builds/Ender-3.js
@@ -23,6 +23,7 @@ module.exports = {
         enable: [
           ["NOZZLE_TO_PROBE_OFFSET", [0, 0, 0]]
           //output => #define NOZZLE_TO_PROBE_OFFSET { 0, 0, 0 }
+          
         ],
         disable: []
     }
