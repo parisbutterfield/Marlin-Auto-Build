0746dfc45fdbddfdcdee5fecef75540341eecea0
nbost130 <nbost130@gmail.com>
2023-01-13 21:38:55 -0500

    BLTOUCH

--- builds/Ender-3 Pro.js
+++ builds/Ender-3 Pro.js
@@ -29,10 +29,10 @@ module.exports = {
             ["PREHEAT_2_TEMP_BED",     70],
             ["PREHEAT_2_TEMP_CHAMBER", 35],
             ["PREHEAT_2_FAN_SPEED",   255],
-            //["S_CURVE_ACCELERATION"],
-            ["INDIVIDUAL_AXIS_HOMING_MENU"]
+            //["S_CURVE_ACCELERATION"], Breaks build in Marlin-Auto-Build on Git
+            //["INDIVIDUAL_AXIS_HOMING_MENU"]
             //BL Touch changes from https://3dprintscape.com/marlin-firmware-on-creality-board-complete-guide/
-            //["BLTOUCH"],
+            ["BLTOUCH"]
             //["NOZZLE_TO_PROBE_OFFSET", [10, 10, 0]],
             //["AUTO_BED_LEVELING_UBL"],
             //["RESTORE_LEVELING_AFTER_G28"],
